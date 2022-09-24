3648279e754008f2226634b1cb671d2515e6f061
ofadiman <ofadiman@gmail.com>
2022-09-24 19:06:08 +0200

    feat: add examples of pure and impure functions

---
+++ src/1-pure-and-impure-functions.ts
@@ -0,0 +1,38 @@
+import knex from 'knex'
+
+const connection = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './database.sqlite',
+  },
+})
+
+const isEven = (value: number) => value % 2 === 0
+
+const repeat = (value: string, times: number): string => {
+  let final = ''
+  for (let i = 0; i++; i < times) {
+    final += value
+  }
+
+  return final
+}
+
+let impure = 0
+const modifyValue = (value: number) => {
+  impure += value
+}
+
+const log = (value: string) => console.log(`My value is ${value}`)
+
+const makeRequest = async () => {
+  return await (await fetch('https://jsonplaceholder.typicode.com/todos/1')).json()
+}
+
+const queryDatabase = () => {
+  return connection.select('*').from('users')
+}
+
+const modifyReference = (value: { foo: string }) => {
+  value.foo = 'bar'
+}
