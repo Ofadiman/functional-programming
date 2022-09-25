e48e101a95d3060716c1589f9df1c484795aa207
ofadiman <ofadiman@gmail.com>
2022-09-25 17:04:43 +0200

    refactor: add namespaces to pure functions examples

--- src/1-pure-and-impure-functions.ts
+++ src/1-pure-and-impure-functions.ts
@@ -1,4 +1,5 @@
 import knex from 'knex'
+import * as fs from 'fs'
 
 const connection = knex({
   client: 'sqlite3',
@@ -7,32 +8,47 @@ const connection = knex({
   },
 })
 
-const isEven = (value: number) => value % 2 === 0
+namespace PureFunctions {
+  const isEven = (value: number) => value % 2 === 0
 
-const repeat = (value: string, times: number): string => {
-  let final = ''
-  for (let i = 0; i++; i < times) {
-    final += value
+  const repeat = (value: string, times: number): string => {
+    let final = ''
+    for (let i = 0; i++; i < times) {
+      final += value
+    }
+
+    return final
+  }
+}
+
+namespace ImpureFunctions {
+  let modified = 0
+  const modifyVariableOutOfScope = (value: number) => {
+    modified += value
   }
 
-  return final
-}
+  let noScope = 0
+  const computeUsingVariableOutOfScope = (value: number) => {
+    return noScope + value
+  }
 
-let impure = 0
-const modifyValue = (value: number) => {
-  impure += value
-}
+  const outputToConsole = (value: string) => {
+    console.log(`My value is ${value}`)
+  }
 
-const log = (value: string) => console.log(`My value is ${value}`)
+  const makeHttpRequest = async () => {
+    return await (await fetch('https://jsonplaceholder.typicode.com/todos/1')).json()
+  }
 
-const makeRequest = async () => {
-  return await (await fetch('https://jsonplaceholder.typicode.com/todos/1')).json()
-}
+  const queryDatabase = () => {
+    return connection.select('*').from('users')
+  }
 
-const queryDatabase = () => {
-  return connection.select('*').from('users')
-}
+  const modifyReference = (value: { foo: string }) => {
+    value.foo = 'bar'
+  }
 
-const modifyReference = (value: { foo: string }) => {
-  value.foo = 'bar'
+  const writeToFileSystem = () => {
+    fs.writeFileSync('./file', 'content')
+  }
 }
